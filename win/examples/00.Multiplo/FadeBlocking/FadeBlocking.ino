d087e89e981a3a42421a47c15e9da8a497f522d9
Chris Newton <northriverloghomes@gmail.com>
2015-02-05 10:10:02 -0800

    changed FadeBlocking to use 'led' instead of 'ledPin'

--- win/examples/00.Multiplo/FadeBlocking/FadeBlocking.ino
+++ win/examples/00.Multiplo/FadeBlocking/FadeBlocking.ino
@@ -1,21 +1,24 @@
 //
 // FadeBlocking
 //
-// fade an LED without PWM ...
+// fade an LED without PWM and analogWrite...
 // uses delay() instead
 //
 // Notes:
-// Assumes an LED is properly connected to pin D9
+//   - Assumes an LED is properly connected to pin D9
+//   - not using analogWrite because PWM not configured on Duinobot
 //
 
-#define ledPin 9
 
 #define DURATION 512
 #define SCALE 40
 
+int led = 9;
+
+
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 
@@ -24,20 +27,20 @@ void loop()
   // fade from on to off
   for (int i = 0; i < DURATION; i++)
   {
-    digitalWrite(ledPin, 0);
+    digitalWrite(led, 0);
     delay(i/SCALE);
   
-    digitalWrite(ledPin, 1);
+    digitalWrite(led, 1);
     delay((DURATION - i)/SCALE);
   }
 
   // glow more nothing to fully on
   for (int i = DURATION; i >= 0 ; i--)
   {
-    digitalWrite(ledPin, 0);
+    digitalWrite(led, 0);
     delay(i/SCALE);
   
-    digitalWrite(ledPin, 1);
+    digitalWrite(led, 1);
     delay((DURATION - i)/SCALE);
   }
 
